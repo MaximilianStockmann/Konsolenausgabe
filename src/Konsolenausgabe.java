f3d241ccad6021c3883933e9823d89328f34f8df
MaximilianStockmann <maximilian.stockmann@gmx.de>
2021-09-14 13:02:23 +0200

    Aufgabe 2 bearbeitet

--- src/Konsolenausgabe.java
+++ src/Konsolenausgabe.java
@@ -1,8 +1,20 @@
 public class Konsolenausgabe {
-    public static void main(String args) {
+    public static void main(String args[]) {
+        //Code für Aufgabe 1
         System.out.print("\"Das ist ein Beispielsatz.\"\n");
         System.out.println("Dies ist ein weiterer " +  "Beispielsatz.");
 
         //Das ist ein Kommentar
+
+        //Code für Aufgabe 2
+        System.out.println("      *      ");
+        System.out.println("     ***     ");
+        System.out.println("    *****    ");
+        System.out.println("   *******   ");
+        System.out.println("  *********  ");
+        System.out.println(" *********** ");
+        System.out.println("*************");
+        System.out.println("     ***     ");
+        System.out.println("     ***     ");
     }
 }
