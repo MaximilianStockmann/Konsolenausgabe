20478e6743edb5f953e4ec654c035bb5baf0174d
MaximilianStockmann <maximilian.stockmann@gmx.de>
2021-09-14 12:55:49 +0200

    Aufgabe 1 bearbeitet

---
+++ src/Konsolenausgabe.java
@@ -0,0 +1,8 @@
+public class Konsolenausgabe {
+    public static void main(String args) {
+        System.out.print("\"Das ist ein Beispielsatz.\"\n");
+        System.out.println("Dies ist ein weiterer " +  "Beispielsatz.");
+
+        //Das ist ein Kommentar
+    }
+}
